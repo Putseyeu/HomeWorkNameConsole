36af887269b8b7320759a5447fb35cdc0d46b0b2
Putseyeu <putseyeu.a.d@gmail.com>
2021-11-14 15:01:59 +0100

    step2

--- Program.cs
+++ Program.cs
@@ -14,19 +14,18 @@ namespace HomeWorkNameConsole
             string nameInput = Console.ReadLine();
             Console.WriteLine("Выберите ваш символ.Пример ! @ # $ % ^ & * )... ");
             string symbol = Console.ReadLine();
-            int symbolChek= Convert.ToInt32(symbol.Length);
+            int symbolChek= symbol.Length;
             int symbolMax = 1;
-            if (symbolChek > symbolMax)
+                       
+            while(symbolChek != symbolMax)
             {
-                while(symbolChek != symbolMax)
-                {
-                    Console.WriteLine("Введите один символ!!!!!!");
-                    symbol = Console.ReadLine();
-                    symbolChek = Convert.ToInt32(symbol.Length);
-                }
+              Console.WriteLine("Введите один символ!!!!!!");
+              symbol = Console.ReadLine();
+              symbolChek = symbol.Length;
             }
+            
 
-            int totalSymbalLine = Convert.ToInt32((nameInput.Length)) + 1;
+            int totalSymbalLine = nameInput.Length + 1;
             for (int symbolLineTop = 0; symbolLineTop < totalSymbalLine; symbolLineTop += 1)
             {
                 Console.Write(symbol);
